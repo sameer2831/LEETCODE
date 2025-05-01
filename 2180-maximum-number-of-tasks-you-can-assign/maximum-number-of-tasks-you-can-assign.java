6988551aa36b52f8d9d51ea2f97d68cd176cc2aa
Sameer Balkawade <43665401+sameer2831@users.noreply.github.com>
2025-04-30 20:35:25 -0400

    Time: 1849 ms (21.22%) | Memory: 56.7 MB (22.04%) - LeetSync

--- 2180-maximum-number-of-tasks-you-can-assign/maximum-number-of-tasks-you-can-assign.java
+++ 2180-maximum-number-of-tasks-you-can-assign/maximum-number-of-tasks-you-can-assign.java
@@ -1,51 +1,61 @@
 class Solution {
-    public int maxTaskAssign(int[] t, int[] w, int p, int s) {
-        int n = w.length;
-        int m = t.length;
-        Arrays.sort(t);
-        
-        int lo = 1, hi = Math.min(n, m);  // hi = Math.min(n, m),  since max tasks you can assign is the minimum of tasks and workers
-        int ans = 0;
-		// binary search to find the max tasks we can assign
-        while(lo <= hi){
-            int mid = (lo+hi)>>1;
-            if(check(t, w, p, s, mid)){ 
+
+    public int maxTaskAssign(
+        int[] tasks,
+        int[] workers,
+        int pills,
+        int strength
+    ) {
+        Arrays.sort(tasks);
+        Arrays.sort(workers);
+        int n = tasks.length, m = workers.length;
+        int left = 1, right = Math.min(m, n), ans = 0;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (check(tasks, workers, pills, strength, mid)) {
                 ans = mid;
-                lo = mid+1;
-            }else{
-                hi = mid-1;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
             }
         }
         return ans;
     }
-    
-    boolean check(int []t, int w[], int p, int s, int n){
-        int idx = n-1;
-        TreeMap<Integer, Integer> map = new TreeMap<>();
-        addAll(w, map);
-        
-        while(idx >= 0){
-            Integer strongest = map.ceilingKey(t[idx]);  // try assigning the task to the strongest worker
-            if(strongest == null){
-                if(p == 0) return false;
-                Integer weekest = map.ceilingKey(t[idx]-s);  // try assigning the task to the weekest worker with pill
-                if(weekest == null) return false; // if cannot assign the task  return false
-                remove(map, weekest);
-                p--;
-            }else{
-                remove(map, strongest);
-            }
-            idx--;
+
+    private boolean check(
+        int[] tasks,
+        int[] workers,
+        int pills,
+        int strength,
+        int mid
+    ) {
+        int p = pills;
+        TreeMap<Integer, Integer> ws = new TreeMap<>();
+        for (int i = workers.length - mid; i < workers.length; ++i) {
+            ws.put(workers[i], ws.getOrDefault(workers[i], 0) + 1);
         }
-        return true;  // if all k tasks are assigned then return true
-    }
-    
-    void addAll(int[] w, TreeMap<Integer, Integer> map){
-        for(int i: w) map.put(i, map.getOrDefault(i, 0) + 1);
-    }
-    
-    void remove(TreeMap<Integer, Integer> map, int val){
-        if(map.get(val) == 1) map.remove(val);
-        else map.put(val, map.get(val)-1);
+        for (int i = mid - 1; i >= 0; --i) {
+            Integer key = ws.lastKey();
+            if (key >= tasks[i]) {
+                ws.put(key, ws.get(key) - 1);
+                if (ws.get(key) == 0) {
+                    ws.remove(key);
+                }
+            } else {
+                if (p == 0) {
+                    return false;
+                }
+                key = ws.ceilingKey(tasks[i] - strength);
+                if (key == null) {
+                    return false;
+                }
+                ws.put(key, ws.get(key) - 1);
+                if (ws.get(key) == 0) {
+                    ws.remove(key);
+                }
+                --p;
+            }
+        }
+        return true;
     }
 }
