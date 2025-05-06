464fa8707bd8e15874210333f8bd28e28edc1c87
Sameer Balkawade <43665401+sameer2831@users.noreply.github.com>
2025-05-06 16:21:04 -0400

    Time: 1 ms (98.74%) | Memory: 45.8 MB (7.64%) - LeetSync

--- 2048-build-array-from-permutation/build-array-from-permutation.java
+++ 2048-build-array-from-permutation/build-array-from-permutation.java
@@ -1,15 +1,10 @@
 class Solution {
-
     public int[] buildArray(int[] nums) {
-        int n = nums.length;
-        // Build the final value on the first iteration
-        for (int i = 0; i < n; ++i) {
-            nums[i] += 1000 * (nums[nums[i]] % 1000);
+        int[] result=new int[nums.length];
+        for(int i=0;i<nums.length;i++)
+        {
+            result[i]=nums[nums[i]];
         }
-        // Modified to final value on the second iteration
-        for (int i = 0; i < n; ++i) {
-            nums[i] /= 1000;
-        }
-        return nums;
+        return result;
     }
 }
