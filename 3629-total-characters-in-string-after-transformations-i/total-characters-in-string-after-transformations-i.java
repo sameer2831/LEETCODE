a9c6d12f614d745ba459cb60fa3fc733bdbf3306
Sameer Balkawade <43665401+sameer2831@users.noreply.github.com>
2025-05-13 13:29:28 -0400

    Time: 14 ms (97.47%) | Memory: 45.3 MB (97.47%) - LeetSync

--- 3629-total-characters-in-string-after-transformations-i/total-characters-in-string-after-transformations-i.java
+++ 3629-total-characters-in-string-after-transformations-i/total-characters-in-string-after-transformations-i.java
@@ -1,26 +1,15 @@
 class Solution {
-    private static final int mod = 1000000007;
-    private int mod_add(int a, int b) {
-        a %= mod; b %= mod;
-        return ((a + b) % mod + mod) % mod;
-    }
+    private final int mod = 1_000_000_007;
     public int lengthAfterTransformations(String s, int t) {
-        int[] nums = new int[26];
-        for (char ch : s.toCharArray()) nums[ch - 'a']++;
-        while (t-- > 0) {
-            int[] cur = new int[26];
-            for (int j = 0; j < 26; j++) {
-                if (j == 25 && nums[j] > 0) {
-                    cur[0] = mod_add(cur[0], nums[j]);
-                    cur[1] = mod_add(cur[1], nums[j]);
-                } else {
-                    if (j < 25) cur[j + 1] = mod_add(cur[j + 1], nums[j]);
-                }
-            }
-            nums = cur;
-        }
+        int[] dp = new int[t + 26];
+        for (int i = 0; i < 26; i++) 
+            dp[i] = 1;
+        for (int i = 26; i < t + 26; i++)   
+            dp[i] = (dp[i - 26] + dp[i - 25]) % mod;
+            
         int ans = 0;
-        for (int i : nums) ans = mod_add(ans, i);
+        for (char ch : s.toCharArray()) 
+            ans = (ans + dp[ch - 'a' + t]) % mod;
         return ans;
     }
 }
