aa969dc51f7f477cbbccdd0ad6a9eb08e7944474
Sameer Balkawade <43665401+sameer2831@users.noreply.github.com>
2025-05-14 12:00:48 -0400

    Time: 163 ms (59.46%) | Memory: 45.3 MB (89.19%) - LeetSync

--- 3630-total-characters-in-string-after-transformations-ii/total-characters-in-string-after-transformations-ii.java
+++ 3630-total-characters-in-string-after-transformations-ii/total-characters-in-string-after-transformations-ii.java
@@ -1,50 +1,69 @@
-class Solution {
-    private static final int mod = 1000000007;
+import java.util.*;
 
-    private long[][] multiplyMatrices(long[][] A, long[][] B) {
-        int rowsA = A.length, colsA = A[0].length, colsB = B[0].length;
-        long[][] result = new long[rowsA][colsB];
-        for (int i = 0; i < rowsA; i++) {
-            for (int j = 0; j < colsB; j++) {
-                long sum = 0;
-                for (int k = 0; k < colsA; k++) {
-                    sum = (sum + (A[i][k] * B[k][j]) % mod) % mod;
-                }
-                result[i][j] = sum;
-            }
-        }
-        return result;
-    }
-
-    private long[][] powerMatrix(long[][] matrix, long exponent) {
-        int n = matrix.length;
-        long[][] result = new long[n][n];
-        for (int i = 0; i < n; i++) result[i][i] = 1;
-        while (exponent > 0) {
-            if ((exponent & 1) == 1) result = multiplyMatrices(result, matrix);
-            matrix = multiplyMatrices(matrix, matrix);
-            exponent >>= 1;
-        }
-        return result;
-    }
+public class Solution {
+    static final int MOD = 1_000_000_007;
 
     public int lengthAfterTransformations(String s, int t, List<Integer> nums) {
-        long[][] transform = new long[26][26];
+        int[][] T = getTransformationMatrix(nums);
+        int[][] poweredT = matrixPow(T, t);
+        int[] count = new int[26];
+        int[] lengths = new int[26];
+
+        for (char c : s.toCharArray()) {
+            count[c - 'a']++;
+        }
+
         for (int i = 0; i < 26; i++) {
-            for (int shift = 0; shift < nums.get(i); shift++) {
-                transform[i][(i + 1 + shift) % 26]++;
+            for (int j = 0; j < 26; j++) {
+                lengths[j] = (int)((lengths[j] + 1L * count[i] * poweredT[i][j]) % MOD);
             }
         }
-        transform = powerMatrix(transform, t);
-        long[][] freq = new long[1][26];
-        for (char ch : s.toCharArray()) {
-            freq[0][ch - 'a']++;
+
+        int total = 0;
+        for (int len : lengths) {
+            total = (total + len) % MOD;
         }
-        freq = multiplyMatrices(freq, transform);
-        long total = 0;
-        for (long cnt : freq[0]) {
-            total = (total + cnt) % mod;
+
+        return total;
+    }
+
+    private int[][] getTransformationMatrix(List<Integer> nums) {
+        int[][] T = new int[26][26];
+        for (int i = 0; i < nums.size(); i++) {
+            for (int step = 1; step <= nums.get(i); step++) {
+                T[i][(i + step) % 26]++;
+            }
         }
-        return (int)total;
+        return T;
+    }
+
+    private int[][] getIdentityMatrix(int size) {
+        int[][] I = new int[size][size];
+        for (int i = 0; i < size; i++) {
+            I[i][i] = 1;
+        }
+        return I;
+    }
+
+    private int[][] matrixMult(int[][] A, int[][] B) {
+        int size = A.length;
+        int[][] C = new int[size][size];
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                long sum = 0;
+                for (int k = 0; k < size; k++) {
+                    sum = (sum + 1L * A[i][k] * B[k][j]) % MOD;
+                }
+                C[i][j] = (int)sum;
+            }
+        }
+        return C;
+    }
+
+    private int[][] matrixPow(int[][] M, int n) {
+        if (n == 0) return getIdentityMatrix(M.length);
+        if (n % 2 == 1) return matrixMult(M, matrixPow(M, n - 1));
+        int[][] half = matrixPow(M, n / 2);
+        return matrixMult(half, half);
     }
 }
