749429226ad726add30873a44f040fc250d9c5a1
Sameer Balkawade <43665401+sameer2831@users.noreply.github.com>
2025-05-16 12:25:45 -0400

    Time: 103 ms (31.71%) | Memory: 44.9 MB (92.68%) - LeetSync

--- 3142-longest-unequal-adjacent-groups-subsequence-ii/longest-unequal-adjacent-groups-subsequence-ii.java
+++ 3142-longest-unequal-adjacent-groups-subsequence-ii/longest-unequal-adjacent-groups-subsequence-ii.java
@@ -1,44 +1,49 @@
-class Solution {
-    public boolean differByOneChar(String word1, String word2) {
-        if (word1.length() != word2.length()) return false;
-        int diffCount = 0;
-        for (int i = 0; i < word1.length(); i++)
-            if (word1.charAt(i) != word2.charAt(i))
-                diffCount++;
-        return diffCount == 1;
-    }
+import java.util.*;
 
+class Solution {
     public List<String> getWordsInLongestSubsequence(String[] words, int[] groups) {
-        int n = groups.length;
+        int n = words.length;
         int[] dp = new int[n];
-        int[] parent = new int[n];
+        int[] prev = new int[n];
+
         Arrays.fill(dp, 1);
-        Arrays.fill(parent, -1);
-        int maxi = 0;
+        Arrays.fill(prev, -1);
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < i; j++) {
-                if (groups[i] != groups[j] &&
-                    differByOneChar(words[i], words[j]) &&
-                    dp[i] < dp[j] + 1) {
-                    dp[i] = dp[j] + 1;
-                    parent[i] = j;
+                if (groups[i] != groups[j] && hamming(words[i], words[j]) == 1) {
+                    if (dp[j] + 1 > dp[i]) {
+                        dp[i] = dp[j] + 1;
+                        prev[i] = j;
+                    }
                 }
             }
-            if (dp[i] > maxi) maxi = dp[i];
         }
 
-        List<String> result = new ArrayList<>();
+        int maxLen = 0;
+        int lastIndex = -1;
         for (int i = 0; i < n; i++) {
-            if (dp[i] == maxi) {
-                while (i != -1) {
-                    result.add(words[i]);
-                    i = parent[i];
-                }
-                break;
+            if (dp[i] > maxLen) {
+                maxLen = dp[i];
+                lastIndex = i;
             }
         }
-        Collections.reverse(result);
+
+        LinkedList<String> result = new LinkedList<>();
+        while (lastIndex != -1) {
+            result.addFirst(words[lastIndex]);
+            lastIndex = prev[lastIndex];
+        }
+
         return result;
+    }
+
+    int hamming(String a, String b) {
+        if (a.length() != b.length()) return 0;
+        int dist = 0;
+        for (int i = 0; i < a.length(); i++) {
+            if (a.charAt(i) != b.charAt(i)) dist++;
+        }
+        return dist;
     }
 }
