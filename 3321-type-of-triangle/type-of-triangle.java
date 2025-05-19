65860851ae5685fc96ed53c649890927fb9d315d
Sameer Balkawade <43665401+sameer2831@users.noreply.github.com>
2025-05-19 12:10:45 -0400

    Time: 0 ms (100.00%) | Memory: 42 MB (60.66%) - LeetSync

--- 3321-type-of-triangle/type-of-triangle.java
+++ 3321-type-of-triangle/type-of-triangle.java
@@ -1,17 +1,23 @@
 class Solution {
     public String triangleType(int[] nums) {
-        HashMap<Integer,Integer> maps=new HashMap<>();
+        
+        String[] res={"equilateral","isosceles","scalene"};
         int a=nums[0],b=nums[1],c=nums[2];
         if(!(a+b>c && b+c>a && c+a>b))
         {
             return "none";
         }
-        String[] res={"equilateral","isosceles","scalene"};
-        for(int i=0;i<3;i++)
+        if(a==b && a==c)
         {
-            maps.put(nums[i],maps.getOrDefault(nums[i],0)+1);
+            return res[0];
         }
-        return res[maps.size()-1];
+        else if(a!=b && b!=c && a!=c)
+        {
+            return res[2];
+        }
+        else{
+            return res[1];
+        }
         
     }
 }
