ab444c91c0ad4719af7b2dadfa38c5cea35e8d96
Sameer Balkawade <43665401+sameer2831@users.noreply.github.com>
2025-06-08 10:30:44 -0400

    Time: 1 ms (100.00%) | Memory: 48.2 MB (88.85%) - LeetSync

--- 386-lexicographical-numbers/lexicographical-numbers.java
+++ 386-lexicographical-numbers/lexicographical-numbers.java
@@ -1,16 +1,18 @@
 class Solution {
     static public List<Integer> lexicalOrder(int n) {
         List<Integer> ans = new ArrayList<>();
-        for (int i = 1; i <= n; i++) {
-            ans.add(i);
-        }
-
-        ans.sort((a, b) -> {
-            String s1 = String.valueOf(a);
-            String s2 = String.valueOf(b);
-            return s1.compareTo(s2);
-        });
-
+        for (int i = 1; i < 10; i++) 
+            solve(i, n, ans);
         return ans;
+    }
+
+    static void solve(int prod, int n, List<Integer> ans) {
+        if (prod > n) return;
+        ans.add(prod);
+        for (int j = 0; j < 10; j++) {
+            int next = prod * 10 + j;
+            if(next > n) return;
+            solve(next, n, ans);
+        }
     }
 }
