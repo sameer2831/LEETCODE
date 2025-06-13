cb3cca1c935ec7ba1663e7be7bd3319df1ea6817
Sameer Balkawade <43665401+sameer2831@users.noreply.github.com>
2025-06-13 15:33:39 -0400

    Time: 8 ms (84.85%) | Memory: 44 MB (67.75%) - LeetSync

--- 326-power-of-three/power-of-three.java
+++ 326-power-of-three/power-of-three.java
@@ -1,11 +1,5 @@
 class Solution {
-    public boolean isPowerOfThree(int n) {
-        if(n==1){
-            return true;
-        }
-        if(n<1 || n%3 != 0){
-            return false;
-        }
-        return isPowerOfThree(n/3);
-    }
+ public boolean isPowerOfThree(int n) {
+       return (Math.log10(n) / Math.log10(3)) % 1 == 0;
+   }
 }
