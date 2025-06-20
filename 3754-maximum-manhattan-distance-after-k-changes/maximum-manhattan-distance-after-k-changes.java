ad8c777560675ab894c7dd75dedf13c01fc163ae
Sameer Balkawade <43665401+sameer2831@users.noreply.github.com>
2025-06-20 11:19:34 -0400

    Time: 58 ms (74.24%) | Memory: 45.8 MB (43.94%) - LeetSync

--- 3754-maximum-manhattan-distance-after-k-changes/maximum-manhattan-distance-after-k-changes.java
+++ 3754-maximum-manhattan-distance-after-k-changes/maximum-manhattan-distance-after-k-changes.java
@@ -4,19 +4,17 @@ class Solution {
         int north = 0, south = 0, east = 0, west = 0;
         
         for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            if (c == 'N') north++;
-            else if (c == 'S') south++;
-            else if (c == 'E') east++;
-            else if (c == 'W') west++;
-            
-            int x = Math.abs(north - south);
-            int y = Math.abs(east - west);
-            int MD = x + y;      //MD =Manhattan Distance
-            int dis = MD + Math.min(2 * k, i + 1 - MD); // Ensure changes are at most k
-            ans = Math.max(ans, dis);
+            char c=s.charAt(i);
+            if(c=='N') north++;
+            else if(c=='S') south++;
+            else if(c=='E') east++;
+            else west++;
+            int x=east-west;
+            int y=north-south;
+            int MD=Math.abs(x)+Math.abs(y);
+            int increase=Math.min(k*2,(i+1)-MD);
+            ans=Math.max(ans,MD+increase);
         }
-        
         return ans;
     }
 }
