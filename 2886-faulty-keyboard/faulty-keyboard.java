d2ee80c1b66843177dcc3c143cfa03a061ce5dc0
Sameer Balkawade <43665401+sameer2831@users.noreply.github.com>
2025-07-02 11:02:49 -0400

    Time: 3 ms (95.19%) | Memory: 45 MB (34.59%) - LeetSync

---
+++ 2886-faulty-keyboard/faulty-keyboard.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String finalString(String s) {
+        StringBuilder res=new StringBuilder();
+        
+        for(int i=0;i<s.length();i++)
+        {
+            if(s.charAt(i)=='i')
+            {
+                res.reverse();
+            }
+            else{
+                res.append(s.charAt(i));
+            }
+        }
+        return res.toString();
+    }
+}
