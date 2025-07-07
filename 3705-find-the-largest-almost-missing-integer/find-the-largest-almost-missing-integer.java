453976d35a8fceb3010cfb0c3a32937602024f36
Sameer Balkawade <43665401+sameer2831@users.noreply.github.com>
2025-07-07 14:54:19 -0400

    Time: 7 ms (9.01%) | Memory: 44.2 MB (52.70%) - LeetSync

--- 3705-find-the-largest-almost-missing-integer/find-the-largest-almost-missing-integer.java
+++ 3705-find-the-largest-almost-missing-integer/find-the-largest-almost-missing-integer.java
@@ -1,51 +1,27 @@
 class Solution {
     public int largestInteger(int[] nums, int k) {
-        int n=nums.length;
-        int max=-1;
-        int maxNum=0;
-        Map<Integer,Integer> freq=new HashMap<>();
-        for(int num:nums)
-        {
-            freq.put(num,freq.getOrDefault(num,0)+1);
-            if(num>maxNum)
-            {
-                maxNum=num;
+        int n = nums.length;
+        Map<Integer, Integer> subarrayCount = new HashMap<>();
+        
+        // Process all subarrays of size k
+        for (int i = 0; i <= n - k; i++) {
+            Set<Integer> uniqueNums = new HashSet<>();
+            for (int j = i; j < i + k; j++) {
+                uniqueNums.add(nums[j]);
+            }
+            for (int num : uniqueNums) {
+                subarrayCount.put(num, subarrayCount.getOrDefault(num, 0) + 1);
             }
         }
-        for(Map.Entry<Integer,Integer> set:freq.entrySet())
-        {
-            if(set.getValue()==1)
-            {
-                max=Math.max(set.getKey(),max);
+        
+        // Find the largest integer that appears in exactly one subarray
+        int ans = -1;
+        for (Map.Entry<Integer, Integer> entry : subarrayCount.entrySet()) {
+            if (entry.getValue() == 1 && entry.getKey() > ans) {
+                ans = entry.getKey();
             }
         }
-        if(k==n)
-        {
-            return maxNum;
-        }
-        if(k==1)
-        {
-            return max;
-        }
-        if(k>1 && k<n)
-        {
-            
-           int x =freq.get(nums[0]), y= freq.get(nums[n-1]);
-           System.out.println("inside K>1 && K<n x: "+x+" y: "+y );
-           if(x>y && y==1)
-           {
-                    return nums[n-1];
-           }
-           if(y>x && x==1)
-           {
-                    return nums[0];
-           }
-           if(x==y && y==1)
-           {
-            return Math.max(nums[0],nums[n-1]);
-           }
-           
-        }
-        return -1;
+        
+        return ans;
     }
 }
