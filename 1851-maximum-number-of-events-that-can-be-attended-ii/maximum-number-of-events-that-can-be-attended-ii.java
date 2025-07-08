f62546d3e8b262c950accc20d4abeb00d041d0ed
Sameer Balkawade <43665401+sameer2831@users.noreply.github.com>
2025-07-08 11:12:54 -0400

    Time: 50 ms (77.88%) | Memory: 121.1 MB (95.19%) - LeetSync

--- 1851-maximum-number-of-events-that-can-be-attended-ii/maximum-number-of-events-that-can-be-attended-ii.java
+++ 1851-maximum-number-of-events-that-can-be-attended-ii/maximum-number-of-events-that-can-be-attended-ii.java
@@ -1,33 +1,56 @@
-class Solution {
-    public int maxValue(int[][] events, int k) {
-        Arrays.sort(events, (a, b) -> a[1] - b[1]); 
+class Solution 
+{
+    public int maxValue(int[][] events, int k) 
+    {
+        // Step 1: Sort events by end day
+        Arrays.sort(events, (a, b) -> Integer.compare(a[1], b[1]));
         int n = events.length;
 
+        // Step 2: Initialize DP table
         int[][] dp = new int[n + 1][k + 1];
 
-        for (int i = 1; i <= n; i++) {
-            int[] event = events[i - 1];
-            int prev = binarySearch(events, event[0]);
+        // Step 3: Loop through events
+        for (int i = 1; i <= n; i++) 
+        {
+            int start = events[i - 1][0];
+            int end = events[i - 1][1];
+            int value = events[i - 1][2];
 
-            for (int j = 1; j <= k; j++) {
-                dp[i][j] = Math.max(dp[i - 1][j], dp[prev + 1][j - 1] + event[2]);
+            // Step 4: Find last event that ends before this one starts
+            int prev = binarySearch(events, i - 1, start);
+
+            for (int j = 1; j <= k; j++) 
+            {
+                // Option 1: Skip current event
+                // Option 2: Take current event + best value from prev non-overlapping event
+                dp[i][j] = Math.max(dp[i - 1][j], dp[prev + 1][j - 1] + value);
             }
         }
+
+        // Step 5: Return max value with k selections
         return dp[n][k];
     }
-    private int binarySearch(int[][] events, int currentStart) {
-        int left = 0, right = events.length - 1;
-        int result = -1;
 
-        while (left <= right) {
+    // Binary Search: find rightmost event with end < start
+    public int binarySearch(int[][] events, int right, int targetStart) 
+    {
+        int left = 0;
+        int res = -1;
+        
+        while (left <= right) 
+        {
             int mid = left + (right - left) / 2;
-            if (events[mid][1] < currentStart) {
-                result = mid;
+            if (events[mid][1] < targetStart) 
+            {
+                res = mid;
                 left = mid + 1;
-            } else {
+            } 
+            else 
+            {
                 right = mid - 1;
             }
         }
-        return result;
+
+        return res;
     }
 }
