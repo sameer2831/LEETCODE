ae50cfbe2fc8772df3113d1447c3bba1f02699d7
Sameer Balkawade <43665401+sameer2831@users.noreply.github.com>
2025-07-10 20:11:10 -0400

    Time: 26 ms (93.01%) | Memory: 51.6 MB (48.81%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,30 +1,33 @@
-public class Solution {
+class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        Arrays.sort(nums);
-        Map<Integer, Integer> count = new HashMap<>();
-        for (int num : nums) {
-            count.put(num, count.getOrDefault(num, 0) + 1);
-        }
-
         List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(nums);
+
         for (int i = 0; i < nums.length; i++) {
-            count.put(nums[i], count.get(nums[i]) - 1);
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            if (i > 0 && nums[i] == nums[i-1]) {
+                continue;
+            }
+            
+            int j = i + 1;
+            int k = nums.length - 1;
 
-            for (int j = i + 1; j < nums.length; j++) {
-                count.put(nums[j], count.get(nums[j]) - 1);
-                if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+            while (j < k) {
+                int total = nums[i] + nums[j] + nums[k];
 
-                int target = -(nums[i] + nums[j]);
-                if (count.getOrDefault(target, 0) > 0) {
-                    res.add(Arrays.asList(nums[i], nums[j], target));
+                if (total > 0) {
+                    k--;
+                } else if (total < 0) {
+                    j++;
+                } else {
+                    res.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    j++;
+
+                    while (nums[j] == nums[j-1] && j < k) {
+                        j++;
+                    }
                 }
             }
-
-            for (int j = i + 1; j < nums.length; j++) {
-                count.put(nums[j], count.get(nums[j]) + 1);
-            }
         }
-        return res;
+        return res;        
     }
 }
