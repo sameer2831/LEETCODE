7d2c5d85b7eb6024feeca97420e61c4f4dba5ffd
Sameer Balkawade <43665401+sameer2831@users.noreply.github.com>
2025-07-10 23:58:11 -0400

    Time: 12 ms (25.59%) | Memory: 44.5 MB (67.90%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.java
+++ 36-valid-sudoku/valid-sudoku.java
@@ -1,68 +1,24 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        
-        for(int x=0;x<9;x+=3)
-        {
-            for(int y=0;y<9;y+=3)
-            {
-                if(!validSquare(board,x,y)) return false;
-                System.out.print(validSquare(board,x,y)+" ");
-            }
-        }
-        
-        System.out.println("");
-        for(int i=0;i<9;i++)
-        {
-            if(!validRow(board,i)) return false;
-            System.out.print(validRow(board,i)+" ");
-        }
-        System.out.println("");
-        for(int i=0;i<9;i++)
-        {
-            if(!validColumn(board,i)) return false;
-            System.out.print(validColumn(board,i)+" ");
-        }
-        
-        return true;
-    }
-    public boolean validRow(char[][] board,int row)
-    {
-        Map<Character,Integer> maps=new HashMap<>();
-        
-        for(int i=0;i<9;i++)
-        {
-               if(board[row][i]=='.') continue;
+        Map<Integer, Set<Character>> cols = new HashMap<>();
+        Map<Integer, Set<Character>> rows = new HashMap<>();
+        Map<String, Set<Character>> squares = new HashMap<>();
 
-                if(maps.containsKey(board[row][i])) return false;
-                maps.put(board[row][i],1);
-        }
-        return true;
-    }
-    public boolean validColumn(char[][] board,int c)
-    {
-        Map<Character,Integer> cmaps=new HashMap<>();
-        
-        for(int i=0;i<9;i++)
-        {
-               if(board[i][c]=='.') continue;
+        for (int r = 0; r < 9; r++) {
+            for (int c = 0; c < 9; c++) {
+                if (board[r][c] == '.') continue;
 
-                if(cmaps.containsKey(board[i][c])) return false;
-                cmaps.put(board[i][c],1);
-        }
-        return true;
-    }
-    public boolean validSquare(char[][] board,int x,int y)
-    {
-        Map<Character,Integer> smaps=new HashMap<>();
-       
-        for(int i=x;i<x+3;i++)
-        {
-            for(int j=y;j<y+3;j++)
-            {
-                if(board[i][j]=='.') continue;
+                String squareKey = (r / 3) + "," + (c / 3);
 
-                if(smaps.containsKey(board[i][j])) return false;
-                smaps.put(board[i][j],1);
+                if (rows.computeIfAbsent(r, k -> new HashSet<>()).contains(board[r][c]) ||
+                    cols.computeIfAbsent(c, k -> new HashSet<>()).contains(board[r][c]) ||
+                    squares.computeIfAbsent(squareKey, k -> new HashSet<>()).contains(board[r][c])) {
+                    return false;
+                }
+
+                rows.get(r).add(board[r][c]);
+                cols.get(c).add(board[r][c]);
+                squares.get(squareKey).add(board[r][c]);
             }
         }
         return true;
