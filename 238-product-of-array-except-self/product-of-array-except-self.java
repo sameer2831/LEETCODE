d256b433dace12099c7ff075f8e749462703a015
Sameer Balkawade <43665401+sameer2831@users.noreply.github.com>
2025-07-10 23:10:39 -0400

    Time: 2 ms (88.21%) | Memory: 54.5 MB (99.88%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -1,41 +1,18 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int totalP=1;
-        
-        int noOf0=0;
-        boolean contains0=false;
-        int n=nums.length;
-        for(int i:nums)
-        {
-            if(i!=0) totalP*=i;
-            else {contains0=true;
-            noOf0++;
-            }
-        }
-        
-        int result[]= new int[n];
+       int n = nums.length;
+        int[] res = new int[n];
 
-        if(noOf0>1)
-        {
-            return result;
-        }
-        if(contains0 )
-        {
-            for(int i=0;i<n;i++)
-            {
-                if(nums[i]==0) result[i]=totalP;
-                else result[i]=0;
-            }
-        }
-        else{
-            for(int i=0;i<n;i++)
-            {
-                
-                result[i]=totalP/nums[i];
-            }
+        res[0] = 1;
+        for (int i = 1; i < n; i++) {
+            res[i] = res[i - 1] * nums[i - 1];
         }
         
-        return result;
-
+        int postfix = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            res[i] *= postfix;
+            postfix *= nums[i];
+        }
+        return res;
     }
 }
