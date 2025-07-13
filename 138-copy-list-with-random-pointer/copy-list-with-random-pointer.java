9cad64556dd852751ef354b3c6b6ac4f38258a18
Sameer Balkawade <43665401+sameer2831@users.noreply.github.com>
2025-07-13 18:37:18 -0400

    Time: 0 ms (100.00%) | Memory: 45 MB (11.98%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.java
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.java
@@ -14,36 +14,16 @@ class Node {
 */
 
 class Solution {
+    HashMap<Node, Node> map = new HashMap<>();
+
     public Node copyRandomList(Node head) {
-        if(head == null) return null;
-        Node curr=head;
-        List<Node> copyNodes=new ArrayList();
-        List<Node> nodes=new ArrayList();
+        if (head == null) return null;
+        if (map.containsKey(head)) return map.get(head);
 
-        while(curr!=null)
-        {
-            int value=curr.val;
-            Node newNode=new Node(value);
-            nodes.add(curr);
-            copyNodes.add(newNode);
-            curr=curr.next;
-        }
-        int randomIndexes[]=new int[nodes.size()];
-        for(int i=0;i<nodes.size();i++)
-        {
-            Node search=nodes.get(i).random;
-            if(search==null) randomIndexes[i]=-1;
-            else randomIndexes[i]=nodes.indexOf(search);
-        }
-        if(randomIndexes[0]==-1) copyNodes.get(0).random=null;
-        else copyNodes.get(0).random=copyNodes.get(randomIndexes[0]);
-        for(int i=1;i<copyNodes.size();i++)
-        {
-            copyNodes.get(i-1).next=copyNodes.get(i);
-            if(randomIndexes[i]==-1) copyNodes.get(i).random=null;
-            else copyNodes.get(i).random=copyNodes.get(randomIndexes[i]);
-        }
-        return copyNodes.get(0);
-
+        Node copy = new Node(head.val);
+        map.put(head, copy);
+        copy.next = copyRandomList(head.next);
+        copy.random = map.get(head.random);
+        return copy;
     }
 }
