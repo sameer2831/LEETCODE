4f3114925fb5050c2f8c71183d7705e65d4f7c8b
Sameer Balkawade <43665401+sameer2831@users.noreply.github.com>
2025-07-12 20:50:50 -0400

    Time: 26 ms (66.18%) | Memory: 57.8 MB (52.50%) - LeetSync

--- 2497-maximum-matching-of-players-with-trainers/maximum-matching-of-players-with-trainers.java
+++ 2497-maximum-matching-of-players-with-trainers/maximum-matching-of-players-with-trainers.java
@@ -1,27 +1,14 @@
 class Solution {
-    public int matchPlayersAndTrainers(int[] players, int[] trainers) {
-        if(players.length==0 || trainers.length ==0) return 0;
-        Arrays.sort(players);
-        Arrays.sort(trainers);
-        int l=0,r=0;
-        int match=0;
-        int n=players.length;
-        int m=trainers.length;
-        while(l<n && r<m)
-        {
-            if(players[l]<=trainers[r])
-            {
-                match++;
-                l++;
-                r++;
-                System.out.println(" l= "+l+" r= "+r+" match = "+match);
+    public int matchPlayersAndTrainers(int[] player, int[] tire) {
+        Arrays.sort(player);
+        Arrays.sort(tire);
+        int i = 0, j = 0, count = 0;
+        while (i < player.length && j < tire.length) {
+            if (player[i] <= tire[j]) {
+                i++; count++;
             }
-            else{
-                r++;
-                System.out.println(" Inside else");
-                System.out.println(" l= "+l+" r= "+r+" match = "+match);
-            }
+            j++;
         }
-        return match;
+        return count;
     }
 }
