61b567e7aa33239c85434b9b69f7264615ca0ce0
Sameer Balkawade <43665401+sameer2831@users.noreply.github.com>
2025-07-14 09:35:23 -0400

    Time: 1 ms (82.89%) | Memory: 42.4 MB (99.56%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -1,27 +1,31 @@
 class Solution {
-    public boolean isValid(String s)
-    {
-        int open=0;
-        for(char c:s.toCharArray())
-        {
-            open+=c=='('?1:-1;
-            if(open<0) return false;
-        }
-        return open==0;
+    public List<String> generateParenthesis(int n) {
+        List<String> answer = new ArrayList<>();
+        backtracking(answer, new StringBuilder(), 0, 0, n);
+
+        return answer;
     }
-    public void dfs(String s,List<String> res,int n)
-    {
-        if(s.length()==n*2)
-        {
-            if(isValid(s)) res.add(s);
+
+    private void backtracking(
+        List<String> answer,
+        StringBuilder curString,
+        int leftCount,
+        int rightCount,
+        int n
+    ) {
+        if (curString.length() == 2 * n) {
+            answer.add(curString.toString());
             return;
         }
-        dfs(s+"(",res,n);
-        dfs(s+")",res,n);
-    }
-    public List<String> generateParenthesis(int n) {
-        List<String> res=new ArrayList<>();
-        dfs("",res,n);
-        return res;
+        if (leftCount < n) {
+            curString.append("(");
+            backtracking(answer, curString, leftCount + 1, rightCount, n);
+            curString.deleteCharAt(curString.length() - 1);
+        }
+        if (leftCount > rightCount) {
+            curString.append(")");
+            backtracking(answer, curString, leftCount, rightCount + 1, n);
+            curString.deleteCharAt(curString.length() - 1);
+        }
     }
 }
