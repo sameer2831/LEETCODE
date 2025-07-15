923e41619386c95d7b7e4b5191aeb6d8293f713c
Sameer Balkawade <43665401+sameer2831@users.noreply.github.com>
2025-07-15 15:58:11 -0400

    Time: 1 ms (26.30%) | Memory: 44.3 MB (85.99%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.java
+++ 110-balanced-binary-tree/balanced-binary-tree.java
@@ -1,30 +1,42 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-class Solution {
-    public int height(TreeNode root)
-    {
-        if(root==null) return 0;
-        return 1+Math.max(height(root.left),height(root.right));
+// Utility class to store information from recursive calls
+final class TreeInfo {
+    public final int height;
+    public final boolean balanced;
+
+    public TreeInfo(int height, boolean balanced) {
+        this.height = height;
+        this.balanced = balanced;
     }
+}
+
+class Solution {
+    // Return whether or not the tree at root is balanced while also storing
+    // the tree's height in a reference variable.
+    private TreeInfo isBalancedTreeHelper(TreeNode root) {
+        // An empty tree is balanced and has height = -1
+        if (root == null) {
+            return new TreeInfo(-1, true);
+        }
+
+        // Check subtrees to see if they are balanced.
+        TreeInfo left = isBalancedTreeHelper(root.left);
+        if (!left.balanced) {
+            return new TreeInfo(-1, false);
+        }
+        TreeInfo right = isBalancedTreeHelper(root.right);
+        if (!right.balanced) {
+            return new TreeInfo(-1, false);
+        }
+
+        // Use the height obtained from the recursive calls to
+        // determine if the current node is also balanced.
+        if (Math.abs(left.height - right.height) < 2) {
+            return new TreeInfo(Math.max(left.height, right.height) + 1, true);
+        }
+        return new TreeInfo(-1, false);
+    }
+
     public boolean isBalanced(TreeNode root) {
-        if(root==null) return true;
-        int left=height(root.left);
-        int right=height(root.right);
-        if(Math.abs(left-right)>1) return false;
-        return isBalanced(root.left) && isBalanced(root.right);
-        
+        return isBalancedTreeHelper(root).balanced;
     }
 }
