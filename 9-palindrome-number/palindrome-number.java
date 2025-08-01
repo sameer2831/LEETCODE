2c8c6df9d47535effe164bd0115190705405f6e2
Sameer Balkawade <43665401+sameer2831@users.noreply.github.com>
2025-08-01 12:53:55 -0400

    Time: 4 ms (100.00%) | Memory: 44.3 MB (35.84%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -1,14 +1,25 @@
-class Solution {
+public class Solution {
     public boolean isPalindrome(int x) {
-        String rev=Integer.toString(x);
-        int l=0;
-        int r=rev.length()-1;
-        while(l<r)
-        {
-            if(rev.charAt(l)!=rev.charAt(r)) return false;
-            l++;
-            r--;
+        // Special cases:
+        // As discussed above, when x < 0, x is not a palindrome.
+        // Also if the last digit of the number is 0, in order to be a palindrome,
+        // the first digit of the number also needs to be 0.
+        // Only 0 satisfy this property.
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
         }
-        return true;
+
+        int revertedNumber = 0;
+        while (x > revertedNumber) {
+            revertedNumber = revertedNumber * 10 + (x % 10);
+            x /= 10;
+        }
+
+        // When the length is an odd number, we can get rid of the middle digit by revertedNumber/10
+        // For example when the input is 12321, at the end of the while loop we get x = 12,
+        // revertedNumber = 123,
+        // since the middle digit doesn't matter in palidrome(it will always equal to itself), we
+        // can simply get rid of it.
+        return x == revertedNumber || x == revertedNumber / 10;
     }
 }
