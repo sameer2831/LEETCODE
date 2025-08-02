cf20ed610d74e638f3feea1e6d3343448fc328dc
Sameer Balkawade <43665401+sameer2831@users.noreply.github.com>
2025-08-02 12:17:18 -0400

    Time: 54 ms (40.84%) | Memory: 67.6 MB (29.58%) - LeetSync

--- 2689-rearranging-fruits/rearranging-fruits.java
+++ 2689-rearranging-fruits/rearranging-fruits.java
@@ -1,31 +1,37 @@
+import java.util.*;
+
 class Solution {
-
     public long minCost(int[] basket1, int[] basket2) {
-        TreeMap<Integer, Integer> freq = new TreeMap<>();
-        int m = Integer.MAX_VALUE;
-        for (int b1 : basket1) {
-            freq.put(b1, freq.getOrDefault(b1, 0) + 1);
-            m = Math.min(m, b1);
-        }
-        for (int b2 : basket2) {
-            freq.put(b2, freq.getOrDefault(b2, 0) - 1);
-            m = Math.min(m, b2);
+        Map<Integer, Integer> totalCounts = new HashMap<>();
+        for (int fruit : basket1) totalCounts.put(fruit, totalCounts.getOrDefault(fruit, 0) + 1);
+        for (int fruit : basket2) totalCounts.put(fruit, totalCounts.getOrDefault(fruit, 0) + 1);
+
+        long minVal = Long.MAX_VALUE;
+        for (Map.Entry<Integer, Integer> entry : totalCounts.entrySet()) {
+            if (entry.getValue() % 2 != 0) return -1;
+            minVal = Math.min(minVal, entry.getKey());
         }
 
-        List<Integer> merge = new ArrayList<>();
-        for (var entry : freq.entrySet()) {
-            int count = entry.getValue();
-            if (count % 2 != 0) return -1;
-            for (int i = 0; i < Math.abs(count) / 2; i++) {
-                merge.add(entry.getKey());
+        List<Long> fruits_to_swap = new ArrayList<>();
+        Map<Integer, Integer> count1 = new HashMap<>();
+        for (int fruit : basket1) count1.put(fruit, count1.getOrDefault(fruit, 0) + 1);
+
+        for (Map.Entry<Integer, Integer> entry : totalCounts.entrySet()) {
+            int fruit = entry.getKey();
+            int diff = count1.getOrDefault(fruit, 0) - (entry.getValue() / 2);
+            for (int i = 0; i < Math.abs(diff); i++) {
+                fruits_to_swap.add((long)fruit);
             }
         }
+        
+        Collections.sort(fruits_to_swap);
 
-        Collections.sort(merge);
-        long res = 0;
-        for (int i = 0; i < merge.size() / 2; i++) {
-            res += Math.min(2 * m, merge.get(i));
+        long totalCost = 0;
+        int swapsToMake = fruits_to_swap.size() / 2;
+        for (int i = 0; i < swapsToMake; i++) {
+            totalCost += Math.min(fruits_to_swap.get(i), 2 * minVal);
         }
-        return res;
+        
+        return totalCost;
     }
 }
